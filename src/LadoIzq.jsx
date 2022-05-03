f9bf12414d3256842687f109452afec03a19f5de
sebacatalan <scatalan2020@alu.uct.cl>
2022-05-03 18:42:24 -0400

    actualización
    actualización

--- src/LadoIzq.jsx
+++ src/LadoIzq.jsx
@@ -3,17 +3,17 @@ import "./css/LadoIzq.css"
 export function LadoIzq(){
     return (
         <div id="nav">
-   <div class="titulonav">
-   Barra de navegación
-   </div>
+            <div class="titulonav">
+                Barra de navegación
+            </div>
    
-   <div class="cuerporec">
-   <ul>
-      <li><a href="/">Home</a></li>
-      <li><a href="/">Perfil</a></li>
-      <li><a href="/">Playlist</a></li>
-   </ul>
-   </div>
-</div>
+        <div class="cuerporec">
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/">Perfil</a></li>
+                <li><a href="/">Playlist</a></li>
+            </ul>
+        </div>
+    </div>
     );
 }
